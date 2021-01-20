04870120a353bf6e77bf97ee2185eafa9e97dd3a
Decryptor <nik_soldat@mail.ru>
2021-01-21 00:02:38 +0900

    Остановился на хэширование паролей
    Signed-off-by: Decryptor <nik_soldat@mail.ru>

--- vendor/login.php
+++ vendor/login.php
@@ -1,3 +1,14 @@
 <?
-    header ('Location: ../index.html')
+    session_start();
+    require_once 'connect.php';
+
+$email = $_POST['email'];
+$password = $_POST['password'];
+$admin = 0;
+
+$check_user = mysqli_query($connect, "SELECT * FROM `users` WHERE `email` = '$email' AND `password` = '$password'");
+if (mysqli_num_rows($check_user) > 0) 
+{
+    echo ('Такой пользователь уже зарегистрирован');
+}
 ?>
--- vendor/register.php
+++ vendor/register.php
@@ -17,7 +17,7 @@ $admin = 0;
 $check_email = mysqli_query($connect, "SELECT * FROM `users` WHERE `email` = '$email'");
 if (mysqli_num_rows($check_email) > 0) 
 {
-    echo ('Такой логин уже существует');
+    echo ('Такой пользователь уже зарегистрирован');
 }
 else 
 {
@@ -47,7 +47,8 @@ else
     }
     if ($email != "" and $lastname != "" and $firstname != "" and $secondname !="" and $password != "" and $passwordConfirm != "" and $password === $passwordConfirm)
     {
-        $sql = "INSERT INTO `users` (`id`, `email`, `lastname`, `firstname`, `secondname`, `bday`, `school`, `classnumber`, `classname`, `password`, `admin`) VALUES (NULL, '$email', '$lastname', '$firstname', '$secondname', '$bday', '$school', '$classnumber', '$classname', '$password', '$admin')";
+        $hash = password_hash($password, PASSWORD_BCRYPT);
+        $sql = "INSERT INTO `users` (`id`, `email`, `lastname`, `firstname`, `secondname`, `bday`, `school`, `classnumber`, `classname`, `password`, `admin`) VALUES (NULL, '$email', '$lastname', '$firstname', '$secondname', '$bday', '$school', '$classnumber', '$classname', '$hash', '$admin')";
         mysqli_query ($connect,$sql);
         //header ('Location: ../register.html');
         echo ("<SCRIPT LANGUAGE='JavaScript'> window.alert('Succesfully Registered'); window.location.href='../index.html'; </SCRIPT>");
