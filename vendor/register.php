5065c6cbb756c0ee12be695d6fa39bc27aa3ad10
Decryptor <nik_soldat@mail.ru>
2021-01-20 23:42:29 +0900

    Сделал регистрацию и добавление в базу данных
    Signed-off-by: Decryptor <nik_soldat@mail.ru>

--- vendor/register.php
+++ vendor/register.php
@@ -12,39 +12,45 @@ $classnumber = $_POST['classnumber'];
 $classname = $_POST['classname'];
 $password = $_POST['password'];
 $passwordConfirm = $_POST['passwordConfirm'];
+$admin = 0;
 
 $check_email = mysqli_query($connect, "SELECT * FROM `users` WHERE `email` = '$email'");
 if (mysqli_num_rows($check_email) > 0) 
 {
     echo ('Такой логин уже существует');
 }
-if ($email === "")
+else 
 {
-    echo ("Введите email");
-}
-if ($lastname === "")
-{
-    echo ("Введите фамилию");
-}
-if ($firstname === "")
-{
-    echo ("Введите имя");
-}
-if ($secondname === "")
-{
-    echo ("Введите отчество");
-}
-if ($password === "")
-{
-    echo ("Введите пароль");
-}
-if ($password != $passwordConfirm)
-{
-    echo ("Пароли не совпадают");
-}
-if ($email != "" and $lastname != "" and $firstname != "" and $secondname !="" and $password != "" and $passwordConfirm != "" and $password === $passwordConfirm)
-{
-    header ('Location: ../register.html');
-   ?> <script>alert ("Регистрация прошла успешно!");</script> <?php
+    if ($email === "")
+    {
+        echo ("Введите email");
+    }
+    if ($lastname === "")
+    {
+        echo ("Введите фамилию");
+    }
+    if ($firstname === "")
+    {
+        echo ("Введите имя");
+    }
+    if ($secondname === "")
+    {
+        echo ("Введите отчество");
+    }
+    if ($password === "")
+    {
+        echo ("Введите пароль");
+    }
+    if ($password != $passwordConfirm)
+    {
+        echo ("Пароли не совпадают");
+    }
+    if ($email != "" and $lastname != "" and $firstname != "" and $secondname !="" and $password != "" and $passwordConfirm != "" and $password === $passwordConfirm)
+    {
+        $sql = "INSERT INTO `users` (`id`, `email`, `lastname`, `firstname`, `secondname`, `bday`, `school`, `classnumber`, `classname`, `password`, `admin`) VALUES (NULL, '$email', '$lastname', '$firstname', '$secondname', '$bday', '$school', '$classnumber', '$classname', '$password', '$admin')";
+        mysqli_query ($connect,$sql);
+        //header ('Location: ../register.html');
+        echo ("<SCRIPT LANGUAGE='JavaScript'> window.alert('Succesfully Registered'); window.location.href='../index.html'; </SCRIPT>");
+    }   
 }
 ?>
